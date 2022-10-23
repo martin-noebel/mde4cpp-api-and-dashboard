712ed6775374aebe12e06b34669079cee9bde3db
Martin Nöbel <martin.noebel1997@gmail.com>
2022-10-23 02:04:46 +0200

    UPDATE: adapted README.md for building and preparing the project and also the script for installing all necessary tools

--- README.md
+++ README.md
@@ -1,17 +1,40 @@
 # Mde4cpp-API and -Dashboard
 
-This is an REST-API inclusive Dashboard for the MDE4CPP-Project.
+This is an REST-API inclusive Dashboard for the MDE4CPP-Project. It uses an library example for a prototype implementation. All necessary files and dlls are included in the project.
 
 ## Preparations
-Download [Boost](https://www.boost.org/users/download/) and make sure to install it in :
-``C:/Program Files/``
+Install all needed tools (CMake, Mingw) and necessary libraries (Boost).
+
+1.) open a powershell as admin and type in:
+```powershell
+set-executionpolicy remotesigned
+```
+
+2.) execute the script with:
+```powershell
+./init.ps1
+```
+
+3.) check cmake and mingw version:
+```powershell
+gcc --version
+cmake --version
+```
 
 ## Building
-1.) configure the build system: ``cmake -B build -S .``
+Build and execute the project and access it at [localhost:8080](http://localhost:8080).
 
-2.) build the project: ``cmake --build build``
+1.) configure the build system:
+```powershell
+cmake -G "MinGW Makefiles" -B build -S .
+```
 
-3.) run the project: ``./build/Mde4cppApi.exe``
+2.) build the project:
+```powershell
+cmake --build build
+```
 
-## Bugs??
-Add the following flags to the ecore CmakeList: ``-std=c++0x -Wno-narrowing``
+3.) run the project:
+```powershell
+./build/Mde4cppApi.exe
+```
--- script.ps1
+++ script.ps1
@@ -1,79 +1,27 @@
-# open a powershell as admin and type in: "set-executionpolicy remotesigned"
-# then run the script with: "./script.ps1"
-# install chocolatey
-echo "Installing Chocolatey"
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-# create a directory under the user folder
-echo "Init MDE4CPP directory"
-mkdir $env:USERPROFILE\MDE4CPP
-cd $env:USERPROFILE\MDE4CPP
-# install java
-echo "Installing Java"
-Invoke-WebRequest "https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.zip" -OutFile java.zip
-Expand-Archive java.zip -DestinationPath .
-rm java.zip
-mv jdk* java
 # install mingw
 echo "Installing Mingw"
 Invoke-WebRequest "https://github.com/brechtsanders/winlibs_mingw/releases/download/12.1.0-14.0.4-10.0.0-ucrt-r2/winlibs-x86_64-posix-seh-gcc-12.1.0-llvm-14.0.4-mingw-w64ucrt-10.0.0-r2.zip" -OutFile mingw.zip
 Expand-Archive mingw.zip -DestinationPath .
 rm mingw.zip
 mv mingw* mingw
-# install gradle
-echo "Installing Gradle"
-Invoke-WebRequest "https://services.gradle.org/distributions/gradle-7.4.2-bin.zip" -OutFile gradle.zip
-Expand-Archive gradle.zip -DestinationPath .
-rm gradle.zip
-mv gradle* gradle
+mv mingw "C:/Program Files/mingw"
+
 # install cmake
 echo "Installing CMake"
 Invoke-WebRequest "https://github.com/Kitware/CMake/releases/download/v3.24.0-rc2/cmake-3.24.0-rc2-windows-x86_64.zip" -OutFile cmake.zip
 Expand-Archive cmake.zip -DestinationPath .
 rm cmake.zip
 mv cmake* cmake
-# set path variable and refresh it
-echo "Setting path variable"
-[Environment]::SetEnvironmentVariable("Path", $env:Path + "$env:USERPROFILE\MDE4CPP\cmake\bin;$env:USERPROFILE\MDE4CPP\gradle\bin;$env:USERPROFILE\MDE4CPP\mingw\bin;$env:USERPROFILE\MDE4CPP\java\bin;", "Machine")
+mv cmake "C:/Program Files/cmake"
+
+# install boost
+echo "Installing Boost"
+Invoke-WebRequest "https://boostorg.jfrog.io/artifactory/main/release/1.79.0/source/boost_1_79_0.zip" -OutFile boost.zip
+Expand-Archive boost.zip -DestinationPath .
+rm boost.zip
+mv boost* boost
+mv boost "C:/Program Files/boost"
+
+# setting path
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\cmake\bin;C:\Program Files\mingw\bin", "Machine")
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
-# install eclipse modeling
-echo "Installing Eclipse Modeling"
-Invoke-WebRequest "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2021-12/R/eclipse-modeling-2021-12-R-win32-x86_64.zip&r=1" -OutFile eclipse.zip
-Expand-Archive eclipse.zip -DestinationPath .
-rm eclipse.zip
-# wait for the user to install all eclipse repositories
-echo "Please install all repositories from: https://github.com/MDE4CPP/UML-Designer"
-./eclipse/eclipse.exe
-Write-Host -NoNewLine 'Press any key to continue if you installed all repositories ...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-# install git
-echo "Installing Git"
-choco install git
-# clone MDE4CPP
-echo "Cloning MDE4CPP repository"
-git clone https://github.com/MDE4CPP/MDE4CPP.git
-mv MDE4CPP src
-# set environment variables and refresh them
-echo "Setting environment variables"
-$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-[Environment]::SetEnvironmentVariable("JAVA_HOME", "$env:USERPROFILE\MDE4CPP\java", "Machine")
-[Environment]::SetEnvironmentVariable("COMPILER_HOME", "$env:USERPROFILE\MDE4CPP\mingw", "Machine")
-[Environment]::SetEnvironmentVariable("GRADLE_HOME", "$env:USERPROFILE\MDE4CPP\gradle", "Machine")
-[Environment]::SetEnvironmentVariable("MDE4CPP_HOME", "$env:USERPROFILE\MDE4CPP\src", "Machine")
-[Environment]::SetEnvironmentVariable("MDE4CPP_ECLIPSE_HOME", "$env:USERPROFILE\MDE4CPP\eclipse", "Machine")
-[Environment]::SetEnvironmentVariable("CMAKE_HOME", "$env:USERPROFILE\MDE4CPP\cmake", "Machine")
-[Environment]::SetEnvironmentVariable("GRADLE_OPTS", "-Dorg.gradle.parallel=true", "Machine")
-[Environment]::SetEnvironmentVariable("ORG_GRADLE_PROJECT_WORKER", "1", "Machine")
-[Environment]::SetEnvironmentVariable("ORG_GRADLE_PROJECT_RELEASE", "1", "Machine")
-[Environment]::SetEnvironmentVariable("ORG_GRADLE_PROJECT_DEBUG", "1", "Machine")
-[Environment]::SetEnvironmentVariable("ORG_GRADLE_PROJECT_DEBUG_MESSAGE_FUML", "0", "Machine")
-[Environment]::SetEnvironmentVariable("CLASSPATH", "$env:USERPROFILE\MDE4CPP\eclipse\plugins", "Machine")
-[Environment]::SetEnvironmentVariable("COMPILER_DELIVERY_NAME", "x86_64-w64-mingw32", "Machine")
-[Environment]::SetEnvironmentVariable("COMPILER_VERSION", "8.1.0", "Machine")
-refreshenv
-# build the project
-echo "Building the project"
-cd src
-gradle
-gradle buildAll
-exit
