2253badf5676ea082ce11473956631b31e3eb406
Martin Nöbel <martin.noebel1997@gmail.com>
2023-01-19 02:53:40 +0100

    ADD: old api as third api type

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,19 @@
 #include "api/libraryModel_genericApi.hpp"
 #include "api/libraryModel_modelApi.hpp"
+#include "api/libraryModel_mixedApi.hpp"
 
 int main ()
 {
-    //Create Model Factory and Model Package
-    std::shared_ptr<libraryModel_ecoreFactory> factory = libraryModel_ecoreFactory::eInstance();
-    std::shared_ptr<libraryModel_ecorePackage> package = libraryModel_ecorePackage::eInstance();
+    //Create Factories and Packages
+    std::shared_ptr<libraryModel_ecoreFactory> libraryFactory = libraryModel_ecoreFactory::eInstance();
+    std::shared_ptr<libraryModel_ecorePackage> libraryPackage = libraryModel_ecorePackage::eInstance();
+    std::shared_ptr<ecoreFactory> ecoreFactory = ecore::ecoreFactory::eInstance();
+    std::shared_ptr<ecorePackage> ecorePackage = ecore::ecorePackage::eInstance();
 
     //Create APIs
-    crow::SimpleApp genericApp;
-    crow::SimpleApp modelApp;
-    std::shared_ptr<libraryModel_genericApi> genericApi = libraryModel_genericApi::eInstance(factory, package, genericApp);
-    std::shared_ptr<libraryModel_modelApi> modelApi = libraryModel_modelApi::eInstance(factory, package, modelApp);
-    auto genericOutput = genericApp.port(8080).multithreaded().run_async();
-    auto modelOutput = modelApp.port(8081).multithreaded().run_async();
+    std::shared_ptr<libraryModel_mixedApi> mixedApi = libraryModel_mixedApi::eInstance(libraryFactory, libraryPackage);
+    //std::shared_ptr<libraryModel_modelApi> modelApi = libraryModel_modelApi::eInstance(libraryFactory, libraryPackage);
+    //std::shared_ptr<libraryModel_genericApi> genericApi = libraryModel_genericApi::eInstance(ecoreFactory, ecorePackage);
 
     return 0;
 }
--- api/libraryModel_genericApi.hpp
+++ api/libraryModel_genericApi.hpp
@@ -1,29 +1,22 @@
 #include "crow/crow_all.h"
 #include "ecore/ecorePackage.hpp"
+#include "ecore/ecoreFactory.hpp"
 #include "ecore/EObject.hpp"
 #include "ecore/EClass.hpp"
 #include "ecore/EStructuralFeature.hpp"
 #include "ecore/EReference.hpp"
-#include "libraryModel_ecore/libraryModel_ecoreFactory.hpp"
-#include "libraryModel_ecore/libraryModel_ecorePackage.hpp"
-#include "libraryModel_ecore/Author.hpp"
-#include "libraryModel_ecore/Book.hpp"
-#include "libraryModel_ecore/Picture.hpp"
-#include "libraryModel_ecore/LibraryModel.hpp"
-#include "libraryModel_ecore/NamedElement.hpp"
 
 using namespace ecore;
-using namespace libraryModel_ecore;
 
 class libraryModel_genericApi{
 public:
-    static std::shared_ptr<libraryModel_genericApi> eInstance(std::shared_ptr<libraryModel_ecoreFactory>& factory, std::shared_ptr<libraryModel_ecorePackage>& package, crow::SimpleApp& app);
+    static std::shared_ptr<libraryModel_genericApi> eInstance(std::shared_ptr<ecoreFactory>& factory, std::shared_ptr<ecorePackage>& package);
     std::shared_ptr<ecore::EObject> readValue(const crow::json::rvalue& content, const std::shared_ptr<ecore::EClass>& eClass);
     crow::json::wvalue writeValue(const std::shared_ptr<ecore::EObject>& object);
 
 private:
-    libraryModel_genericApi(std::shared_ptr<libraryModel_ecoreFactory>& factory, std::shared_ptr<libraryModel_ecorePackage>& package, crow::SimpleApp& app);
-    std::shared_ptr<libraryModel_ecoreFactory> m_factory;
-    std::shared_ptr<libraryModel_ecorePackage> m_package;
+    libraryModel_genericApi(std::shared_ptr<ecoreFactory>& factory, std::shared_ptr<ecorePackage>& package);
+    std::shared_ptr<ecoreFactory> m_factory;
+    std::shared_ptr<ecorePackage> m_package;
     std::map<std::string,std::shared_ptr<ecore::EObject>> m_objects{};
 };
